17501c8070ae0e06e5afefc95102b09e7032513b
root <root@example.com>
2017-12-19 11:02:25 +0900

    編集と削除機能まで実装

--- app_name/app/controllers/productlist_controller.rb
+++ app_name/app/controllers/productlist_controller.rb
@@ -2,7 +2,9 @@
 class ProductlistController < ApplicationController
   protect_from_forgery :except => [:create_product,:create_sale]
   def show
-    @products = Product.all
+    #@products = Product.joins("JOIN sales ON products.product_id = sales.product_id").order(:product_id).select('products.*,sales.month,sales.num').all
+    @products = Product.order(:product_id).all
+    @sales = Sale.order(:month).all
   end
   def new_product 
     @products = Product.new
@@ -12,7 +14,7 @@ class ProductlistController < ApplicationController
     if @products.save
       redirect_to :action => 'show'
     else
-      render plain: "新規登録失敗#{@products.errorsi.messages}"
+      render plain: "新規登録失敗#{@products.errors.messages}"
     end
   end
   def new_sale
@@ -27,14 +29,14 @@ class ProductlistController < ApplicationController
     end
   end
   def edit_product
-    @products = Product.find(params[:product_id])
+    @products = Product.find_by(product_id: params[:product_id])
   end
   def update_product
-    @products = Product.find(params[:product_id])
+    @products = Product.find_by(product_id: params[:product_id])
     if @products.update_attributes(:product_id => params[:product_id],:name => params[:name],:price => params[:price])
       redirect_to :action => 'show'
     else
-      render plain: "更新失敗#{@products.errorsi.messages}:#{params[:product_id]}"
+      render plain: "更新失敗#{@products.errors.messages}:#{params[:product_id]}"
     end
   end
   def edit_sale
@@ -45,7 +47,31 @@ class ProductlistController < ApplicationController
     if @sales.update_attributes(:product_id => params[:product_id],:month => params[:month],:num => params[:num])
       redirect_to :action => 'show'
     else
-      render plain: "更新失敗#{@sales.errorsi.messages}:#{params[:product_id]} #{params[:product_id]}月"
+      render plain: "更新失敗#{@sales.errors.messages}:#{params[:product_id]} #{params[:product_id]}月"
+    end
+  end
+  def destroy_product
+    @products = Product.find_by(product_id: params[:product_id])
+    if @products.destroy 
+      cnt=Sale.where(product_id: params[:product_id]).count
+      if cnt > 0
+        @sales = Sale.where(product_id: params[:product_id]).all
+        if @sales.destroy_all
+        else
+          render plain: "売上一括削除失敗#{@sales.errors.messages}:#{params[:product_id]}"
+        end
+      end
+      redirect_to :action => 'show'
+    else
+      render plain: "商品削除失敗#{@products.errors.messages}:#{params[:product_id]}"
+    end
+  end
+  def destroy_sale
+    @sales = Sale.find_by(product_id: params[:product_id] , month: params[:month])
+    if @sales.destroy
+      redirect_to :action => 'show'
+    else
+      render plain: "削除失敗#{@sales.errors.messages}:#{params[:product_id]} #{params[:product_id]}月"
     end
   end
 end
--- app_name/config/routes.rb
+++ app_name/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'productlist/show'
   get 'productlist/new_product'
   post 'productlist/create_product'
@@ -8,5 +9,6 @@ Rails.application.routes.draw do
   post 'productlist/update_product'
   get 'productlist/edit_sale/:product_id/:month' => 'productlist#edit_sale'
   post 'productlist/update_sale'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  get 'productlist/destroy_product/:product_id' => 'productlist#destroy_product'
+  get 'productlist/destroy_sale/:product_id/:month' => 'productlist#destroy_sale'
+ end
